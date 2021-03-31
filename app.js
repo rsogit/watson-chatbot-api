2b50429fee981e287d43e6043347edbe34e27198
Raul Oliveira <rso@ic.ufal.br>
2021-03-31 09:55:10 -0300

    app.use(bodyParser.urlencoded({

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+  }));
 
 const port = 3000;
 
